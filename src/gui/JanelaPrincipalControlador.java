23ea1239dde8d78e115d9f7994daf0fcaafc60e7
Wilton Castro <wilton_alt@hotmail.com>
2021-03-13 10:07:27 -0300

    JanelaPrincipal/JanelaPrincipalControlador

---
+++ src/gui/JanelaPrincipalControlador.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class JanelaPrincipalControlador implements Initializable{
+
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	@FXML
+	public void onMenuItemVendedorAction() {
+		System.out.println("onMenuItemVendedorAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		System.out.println("onMenuItemDepartamentoAction");
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		System.out.println("onMenuItemSobreAction");
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+	}
+
+}
