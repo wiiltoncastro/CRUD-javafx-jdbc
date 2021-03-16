725ce179471adc05fb9750dd96f72056349d4615
Wilton Castro <wilton_alt@hotmail.com>
2021-03-16 15:57:07 -0300

    DepartamentoFormato (design)

--- src/gui/DepartamentoListaControlador.java
+++ src/gui/DepartamentoListaControlador.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alertas;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entidades.Departamento;
 import model.servicos.DepartamentoServico;
@@ -43,8 +52,9 @@ public class DepartamentoListaControlador implements Initializable{
 	
 	//metodos
 	@FXML
-	public void onBotaoNovoAction() {
-		System.out.println("onBotaoNovoAction");
+	public void onBotaoNovoAction(ActionEvent evento) {
+		Stage parentStage = Utils.palcoAtual(evento);
+		criarFormatoDeDialogo("/gui/DepartamentoFormato.fxml", parentStage);
 	}
 	
 	@Override
@@ -68,4 +78,24 @@ public class DepartamentoListaControlador implements Initializable{
 		obsLista = FXCollections.observableArrayList(listaDepartamento);
 		tableViewDepartamentos.setItems(obsLista);
 	}
+	
+	private void criarFormatoDeDialogo(String nomeAbsoluto, Stage parentStage) {
+		try {
+			FXMLLoader carregador = new FXMLLoader(getClass().getResource(nomeAbsoluto));
+			Pane painel = carregador.load();
+			
+			Stage dialogoPalco = new Stage();
+			dialogoPalco.setTitle("Dados do Departamento");
+			dialogoPalco.setScene(new Scene(painel));
+			dialogoPalco.setResizable(false);
+			dialogoPalco.initOwner(parentStage);
+			dialogoPalco.initModality(Modality.WINDOW_MODAL);
+			dialogoPalco.showAndWait();
+					
+		}
+		catch(IOException e) {
+			Alertas.showAlert("IO Exceção", "Error! Carregando Janela", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 }
